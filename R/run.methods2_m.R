b0bbd598a035d68b3abf5d2cf35fca1acf199a76
Olivia <oliviabiany@hotmail.com>
2025-12-17 16:44:11 -0500

    Update run.methods2_m.R
    function name change

--- R/run.methods2_m.R
+++ R/run.methods2_m.R
@@ -6,7 +6,7 @@
 #'
 #' @export run.methods2
 #'
-run.methods2 <- function (simdata = NULL,
+run.methods2_m <- function (simdata = NULL,
                           n_t = 100,
                           n_v.t = 1,
                           conf.num.vec = c(W = 0, Z = 0, U = 200, K = 0, I = 100),
